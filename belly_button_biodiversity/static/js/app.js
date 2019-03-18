60bc0644a9ad457a572f5c5cedb7439b61469038
Nandini Sridhar <43830438+nsridhar26@users.noreply.github.com>
2019-03-17 21:34:38 -0400

    Add files via upload

--- belly_button_biodiversity/static/js/app.js
+++ belly_button_biodiversity/static/js/app.js
@@ -1,80 +1,72 @@
 function buildMetadata(sample) {
-  var sample = `/metadata/${sample}`
-  Plotly.d3.json(sample,function(error,response){
-    if (error) return console.log(error);
-    console.log(response);
-    var data = response[0];
-    console.log(data)
-    var metaList = document.getElementById('sampleMetadata');
-    metaList.innerHTML = '';
-    var metaItems = [["Sample","SAMPLEID"],["Ethnicity","ETHNICITY"],["Gender","GENDER"],["Age","AGE"],
-        ["Weekly Wash Frequency","WFREQ"],["Type (Innie/Outie)","BBTYPE"],["Country","COUNTRY012"],["Dog Owner","DOG"],["Cat Owner","CAT"]];
-    console.log(metaList)
-    for(i=0; i<metaItems.length; i++){
-        var newLi = document.createElement('li');
-        newLi.innerHTML = `${metaItems[i][0]}: ${data[metaItems[i][1]]}`;
-        metaList.appendChild(newLi);
-    };
+  d3.json(`/metadata/${sample}`).then((data) => {
+  // Use d3 to select the panel with id of `#sample-metadata`
+    var panel = d3.select("#sample-metadata");
+   
+   // Use `.html(“”) to clear any existing metadata
+    panel.html("");
+   
+  // Use `Object.entries` to add each key and value pair to the panel
+  // Hint: Inside the loop, you will need to use d3 to append new
+  // tags for each key-value in the metadata.
+    Object.entries(data).forEach(([key, value]) => {
+      panel.append("h4").text(`${key}: ${value}`);
+      console.log(key, value);
+    });
+  
   });
-};
-
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
 }
 
 function buildCharts(sample) {
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-  d3.select("")
-
-    // @TODO: Build a Bubble Chart using the sample data
-    // var = [sample_data]
-    var trace1 = {
-      x: data.map(row => row.otu_ids),
-      y: data.map(row => row.sample_values),
+  //Use `d3.json` to fetch the sample data for the plots
+  d3.json(`/samples/${sample}`).then((data)=>{
+    var otu_ids=data.otu_ids;
+    var otu_labels=data.otu_labels;
+    var sample_values=data.sample_values;
+    // Build a Bubble Chart using the sample data
+    var trace1 = [{
+      // x: data.map(row => row.otu_ids),
+      // y: data.map(row => row.sample_values),
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
       mode: 'markers',
       marker: {
         color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
         // opacity: [1, 0.8, 0.6, 0.4],
-        size: [sample_values]
+        size: sample_values
       }
-    };
-    
-    var data = [trace1];
+    }];
     
     var layout = {
-      title: 'Marker Size and Color',
+      margin: {t: 0},
       showlegend: false,
       height: 600,
-      width: 600
+      width: 600,
+      hovermode: "closest"
     };
     
-    Plotly.newPlot('bubble', data, layout);
+    Plotly.newPlot('bubble', trace1, layout);
 
 
-    // @TODO: Build a Pie Chart
+    //Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-  // var top_10=
-  var trace1 = {
-  labels: [top_10.label],
-  values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+  var trace2 = [{
+  labels: otu_ids.slice(0, 10),
+  values: sample_values.slice(0, 10),
+  hovertext: otu_labels.slice(0, 10),
+  hoverinfo: "hovertext",
   type: 'pie'
+  }
+];
+
+var layout2 = {
+  margin:{t:0, l:0}
 };
 
-var data = [trace1];
-
-Plotly.newPlot("pie", data);
+Plotly.newPlot('pie', trace2, layout2);
+});
 }
 
 function init() {
@@ -88,6 +80,7 @@ function init() {
         .append("option")
         .text(sample)
         .property("value", sample);
+        console.log(selector)
     });
 
     // Use the first sample from the list to build the initial plots
